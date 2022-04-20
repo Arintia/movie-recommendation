b962ddf8fcdfc9dae357dbce581350604e95cee2
Arintia <arantiabusiness@gmail.com>
2022-04-20 15:45:36 +0300

    Add pagination

--- src/routes/AdminList.js
+++ src/routes/AdminList.js
@@ -5,15 +5,20 @@ import AdminCard from "../components/AdminCard/AdminCard";
 import { faCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { confirmMovie, removeMovie } from "../redux/movies/MoviesSlice";
+import { ITEMS_PER_PAGE } from "../utils/constants";
 
 export default function AdminList() {
   let navigate = useNavigate();
   const dispatch = useDispatch();
   const adminItems = useSelector(state => state.movies.adminItems);
   const isLoggedIn = useSelector(state => state.movies.isLoggedIn);
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [isAllChecked, setIsAllChecked] = useState(false);
   const [checked, setChecked] = useState([]);
   const checkboxes = Array.from(document.getElementsByClassName("input-checkbox"));
+  const [increaseBtn, setIncreaseBtn] = useState();
+  const [decreaseBtn, setDecreaseBtn] = useState();
 
   function addList(id) {
     if(!checked.includes(id)) {
@@ -52,11 +57,66 @@ export default function AdminList() {
     }
   }
 
+  const decreasePage = () => {
+    if(page > 0) {
+      setPage(page - 1);
+    }
+  }
+
+  const increasePage = () => {
+    if(page+1 < totalPages) {
+      setPage(page + 1);
+    }
+  }
+
+  const handlePageSwitch = (e) => {
+    const pageValue = Number(e.target.id);
+    console.log(pageValue);
+    console.log(page);
+    if(pageValue !== page) {
+      setPage(pageValue);
+    }
+  }
+
   useEffect(() => {
     if(!isLoggedIn) {
       navigate("/admin");
     }
   }, [navigate, isLoggedIn]);
+
+  useEffect(() => {
+    if(decreaseBtn) {
+      if(page === 0) {
+        decreaseBtn.disabled = true;
+      }
+      else {
+        decreaseBtn.disabled = false;
+      }
+    }
+    setChecked([]);
+  }, [page, decreaseBtn]);
+
+  useEffect(() => {
+    if(increaseBtn) {
+      if(page + 1 >= totalPages) {
+        increaseBtn.disabled = true;
+      } 
+      else {
+        increaseBtn.disabled = false;
+      }
+    } 
+  }, [page, increaseBtn, totalPages]);
+
+  useEffect(() => {
+    setTotalPages(adminItems.length / ITEMS_PER_PAGE);
+  }, [adminItems.length]);
+
+  useEffect(() => {
+    setIncreaseBtn(document.getElementById("increase-btn"));
+    setDecreaseBtn(document.getElementById("decrease-btn"));
+  }, []);
+
+  const selectedItems = adminItems.slice(((ITEMS_PER_PAGE * page)), (ITEMS_PER_PAGE * (page + 1)));
   
   return (
     <main className="flex flex-col justify-center pt-12 w-screen h-screen bg-slate-100 text-center">
@@ -80,7 +140,7 @@ export default function AdminList() {
             </tr>
           </thead>
           <tbody>
-            {adminItems.map(movie => 
+            {selectedItems.map(movie => 
               <AdminCard 
                 key={movie.id}
                 id={movie.id}
@@ -103,7 +163,36 @@ export default function AdminList() {
                 </label>
               </th>
               <th></th>
-              <th></th>
+              <th className="flex justify-center">
+                <div className="btn-group">
+                  <button 
+                    id="decrease-btn"
+                    className="btn bg-slate-100 text-slate-900 hover:bg-slate-200"
+                    onClick={decreasePage}
+                  >
+                      «
+                  </button>
+                  <button 
+                    className="btn bg-slate-100 text-slate-900 hover:bg-slate-200 dropdown"
+                  >
+                    <label tabIndex="0" className="m-1">Page {page+1}</label>
+                    {totalPages > 0 && 
+                      <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                        {[...Array(Math.ceil(totalPages))].map((page, index) => 
+                          <li><button onClick={handlePageSwitch} id={index}>Page {index+1}</button></li>
+                        )}
+                      </ul>
+                    }
+                  </button>
+                  <button 
+                    id="increase-btn"
+                    className="btn bg-slate-100 text-slate-900 hover:bg-slate-200"
+                    onClick={increasePage}
+                  >
+                    »
+                  </button>
+                </div>
+              </th>
               <th></th>
               <th></th>
               <th>
