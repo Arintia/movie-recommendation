b4ae3325246a8bd147278bb84e549fd0940a85de
Arintia <arantiabusiness@gmail.com>
2022-04-22 13:19:47 +0300

    Remove unused hooks

--- client/src/routes/AdminList.js
+++ client/src/routes/AdminList.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import AdminCard from "../components/AdminCard/AdminCard";
